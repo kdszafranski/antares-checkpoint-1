9e70cf8660ab09a6bcd898531f8e3d0753fb86a0
Kris Szafranski <kdszafranski@gmail.com>
2017-06-26 15:59:12 -0500

    complete walkthru

---
+++ client.js
@@ -0,0 +1,40 @@
+var clickCount = 0;
+
+$(document).ready(function() {
+  console.log('ready to roll');
+  // append starting button here
+
+  // event listeners
+  $("#generateDiv").on('click', generateDiv);
+  $("#container").on('click', '.swapColors', swapColors);
+  $("#container").on('click', '.removeDiv', removeDiv);
+
+});
+
+// appends a new div onto the dom with the current click count
+function generateDiv() {
+  console.log('generate Div');
+  clickCount++;
+
+  $("#container").append('<div></div>');
+  var $el = $("#container").children().last();
+  $el.data('color', $el.css("background-color"));
+
+
+  $el.append('<p>' + clickCount + '</p>');
+  $el.append('<button class="swapColors">Swap</button>');
+  $el.append('<button class="removeDiv">Delete</button>');
+
+}
+
+// changes selected div to yellow
+function swapColors() {
+  // make it yellow
+  $(this).parent().toggleClass('yellow');
+  
+}
+
+// remove selected div from the Dom
+function removeDiv() {
+  $(this).parent().remove();
+}
